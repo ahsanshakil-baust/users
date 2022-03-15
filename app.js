52c07f4a4e8c3aea1d2193c38dbc1a7cee8683f4
Ahsan Ullah Shakil <ahsanshakil.baust@gmail.com>
2022-03-15 22:09:20 +0600

    *

--- app.js
+++ app.js
@@ -35,8 +35,10 @@ app.use(express.urlencoded({ extended: true }));
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "frontend/build")));
 
-    app.get("/", (req, res) => {
-        res.render(path.join(__dirname, "frontend/build/index.html"));
+    app.get("*", (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname, "frontend", "build", "index.html")
+        );
     });
 } else {
     app.get("/", (req, res) => {
