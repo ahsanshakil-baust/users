51f58f16a9ea3a154261f1c7ccbe678faf8a625c
Ahsan Ullah Shakil <ahsanshakil.baust@gmail.com>
2022-03-15 22:13:31 +0600

    again

--- app.js
+++ app.js
@@ -36,9 +36,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "frontend/build")));
 
     app.get("*", (req, res) => {
-        res.sendFile(
-            path.resolve(__dirname, "frontend", "build", "index.html")
-        );
+        res.render(path.join(__dirname, "frontend/build/index.html"));
     });
 } else {
     app.get("/", (req, res) => {
